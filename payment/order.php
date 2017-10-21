40e19ff7202c9fc9d722e1120618387305b9f843
sarahhodgesadyen <32971089+sarahhodgesadyen@users.noreply.github.com>
2017-10-20 17:13:42 -0700

    nl-nl

--- payment/order.php
+++ payment/order.php
@@ -42,7 +42,7 @@ $reference = 'order_id';
 $shopperReference = 'example_shopper';
 
 /** @var $shopperLocale - The shopper locale */
-$shopperLocale = 'en-US';
+$shopperLocale = 'nl-NL';
 
 /** @var $countryCode - The countrycode influences the returned payment methods */
 $countryCode = 'NL';
